4062b2493c0fc2bf20d01351f34617837baafd94
Fredrik Lönnblad <f.lonnblad@gmail.com>
2015-07-24 11:01:33 +0200

    fixed a problem regarding writeheader

--- etag/etag.go
+++ etag/etag.go
@@ -1,33 +1,35 @@
-// Package etag implements an etag handler middleware for Negroni.
+//Package etag implements an etag handler middleware for Negroni.
 package etag
 
 import (
 	"crypto/md5"
 	"fmt"
 	"net/http"
-
-	"github.com/codegangsta/negroni"
 )
 
 type etagResponseWriter struct {
-	nrw negroni.ResponseWriter
-	r   *http.Request
+	writer http.ResponseWriter
+	req    *http.Request
+	code   int
 }
 
-func (erw etagResponseWriter) Write(b []byte) (int, error) {
+func (erw *etagResponseWriter) Write(b []byte) (int, error) {
 	etag := fmt.Sprintf("%x", md5.Sum(b))
-	erw.Header().Add("etag", etag)
+	erw.Header().Set("ETag", etag)
 	if erw.r.Header.Get("If-None-Match") == etag {
-		erw.nrw.WriteHeader(304)
-		return erw.nrw.Write(nil)
+		erw.writer.WriteHeader(304)
+		return erw.writer.Write(nil)
 	}
-	return erw.nrw.Write(b)
+	erw.writer.WriteHeader(erw.code)
+	return erw.writer.Write(b)
 }
-func (erw etagResponseWriter) Header() http.Header {
-	return erw.nrw.Header()
+
+func (erw *etagResponseWriter) Header() http.Header {
+	return erw.writer.Header()
 }
-func (erw etagResponseWriter) WriteHeader(code int) {
-	erw.nrw.WriteHeader(code)
+
+func (erw *etagResponseWriter) WriteHeader(code int) {
+	erw.code = code
 }
 
 type handler struct{}
@@ -36,9 +38,7 @@ func Etag() *handler {
 	return &handler{}
 }
 
-func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	nrw := negroni.NewResponseWriter(w)
-	erw := etagResponseWriter{nrw, r}
-
-	next(erw, r)
+func (h *handler) ServeHTTP(writer http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+	erw := &etagResponseWriter{writer, 200, req}
+	next(erw, req)
 }
